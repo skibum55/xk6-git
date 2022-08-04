fe94147e3b06ae88bed40628e345e7ce92b4a831
Sean Keery <skibum55@hotmail.com>
2022-08-04 16:39:18 -0600

    Using Redis as the baseline
    https://k6.io/blog/extending-k6-with-xk6/

---
+++ git.go
@@ -0,0 +1,46 @@
+package redis
+
+import (
+    "context"
+    "time"
+
+     git "github.com/libgit2/git2go"
+
+    "go.k6.io/k6/js/common"
+    "go.k6.io/k6/js/modules"
+)
+
+// Register the extension on module initialization, available to
+// import from JS as "k6/x/git".
+func init() {
+    modules.Register("k6/x/git", new(Git))
+}
+
+// is the k6 extension for a Git client.
+type Git struct{}
+
+// Client is the Redis client wrapper.
+type Client struct {
+    client *Git.Client
+}
+
+// XClient represents the Client constructor (i.e. `new git.Client()`) and
+// returns a new Git client object.
+func (r *Redis) XClient(ctxPtr *context.Context, opts *redis.Options) interface{} {
+    rt := common.GetRuntime(*ctxPtr)
+    return common.Bind(rt, &Client{client: redis.NewClient(opts)}, ctxPtr)
+}
+
+// Set the given key with the given value and expiration time.
+func (c *Client) Set(key, value string, exp time.Duration) {
+    c.client.Set(c.client.Context(), key, value, exp)
+}
+
+// Get returns the value for the given key.
+func (c *Client) Get(key string) (string, error) {
+    res, err := c.client.Get(c.client.Context(), key).Result()
+    if err != nil {
+        return "", err
+    }
+    return res, nil
+}g
