6b6932fd58ca792d0b81b10e63411cd7e5b3563a
Sean Keery <skibum55@hotmail.com>
2022-08-05 12:34:58 -0600

    git-go function

--- git.go
+++ git.go
@@ -16,31 +16,67 @@ func init() {
     modules.Register("k6/x/git", new(Git))
 }
 
-// is the k6 extension for a Git client.
-type Git struct{}
+// GIT is the k6 extension for a Git client.
+type GIT struct{}
 
-// Client is the Redis client wrapper.
-type Client struct {
-    client *Git.Client
-}
+/ // Repository is the GIT object we'll start with.
+// type Repository struct {
+// 	Storer storage.Storer
+// }
 
-// XClient represents the Client constructor (i.e. `new git.Client()`) and
-// returns a new Git client object.
-func (r *Redis) XClient(ctxPtr *context.Context, opts *redis.Options) interface{} {
-    rt := common.GetRuntime(*ctxPtr)
-    return common.Bind(rt, &Client{client: redis.NewClient(opts)}, ctxPtr)
-}
+    url := "ssh://git@localhost/test_repo.git"
+	directory := "~"
+    publicKeys:= "~/.ssh/gitlab_rsa"
+// Clone gets a git repository
+func (*GIT) PlainClone(directory string, false bool, options &git.CloneOptions{
+		Auth:     publicKeys,
+		URL:      url,
+	})
+	//CheckIfError(err)
 
-// Set the given key with the given value and expiration time.
-func (c *Client) Set(key, value string, exp time.Duration) {
-    c.client.Set(c.client.Context(), key, value, exp)
-}
+// 	// ... retrieving the branch being pointed by HEAD
+// 	ref, err := r.Head()
+// 	CheckIfError(err)
+// 	// ... retrieving the commit object
+// 	commit, err := r.CommitObject(ref.Hash())
+// 	CheckIfError(err)
 
-// Get returns the value for the given key.
-func (c *Client) Get(key string) (string, error) {
-    res, err := c.client.Get(c.client.Context(), key).Result()
-    if err != nil {
-        return "", err
-    }
-    return res, nil
-}
+// 	fmt.Println(commit)
+// func (*GIT) Clone(storer *git.storer, fs, &git.CloneOptions) *git.Clone {
+// 	URL: "https://github.com/git-fixtures/basic.git",
+// }
+
+// // Set adds a key/value
+// func (*REDIS) Set(client *redis.Client, key string, value interface{}, expiration time.Duration) {
+// 	// TODO: Make expiration configurable. Or document somewhere the unit.
+// 	err := client.Set(context.Background(), key, value, expiration*time.Second).Err()
+// 	if err != nil {
+// 		ReportError(err, "Failed to set the specified key/value pair")
+// 	}
+// }
+// 	_, err := os.Stat(privateKeyFile)
+// 	if err != nil {
+// 		Warning("read file %s failed %s\n", privateKeyFile, err.Error())
+// 		return
+// 	}
+
+// // XClient represents the Client constructor (i.e. `new git.Client()`) and
+// // returns a new Git client object.
+// func (r *Redis) XClient(ctxPtr *context.Context, opts *redis.Options) interface{} {
+//     rt := common.GetRuntime(*ctxPtr)
+//     return common.Bind(rt, &Client{client: redis.NewClient(opts)}, ctxPtr)
+// }
+
+// // Set the given key with the given value and expiration time.
+// func (c *Client) Set(key, value string, exp time.Duration) {
+//     c.client.Set(c.client.Context(), key, value, exp)
+// }
+
+// // Get returns the value for the given key.
+// func (c *Client) Get(key string) (string, error) {
+//     res, err := c.client.Get(c.client.Context(), key).Result()
+//     if err != nil {
+//         return "", err
+//     }
+//     return res, nil
+// }
