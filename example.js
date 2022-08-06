78478de8342a73f9c611c8882c2ae87c467fcccd
Sean Keery <skibum55@hotmail.com>
2022-08-06 03:36:41 +0000

    stubbed out a http function in the example

--- example.js
+++ example.js
@@ -1,7 +1,10 @@
 import git from 'k6/x/git';
 
 export default function () {
+  let r = (Math.random() + 1).toString(36).substring(7);
+  var folder = "myfolder" + r
+  console.log(folder)
   // console.log("Debug test")
-  // console.log(git.plainClone("","","somewhere"))
-  console.log(git.plainClone("","ssh://git@0.0.0.0:22222/xk6-git","/home/codespace/.ssh/id_rsa"))
+  // console.log(git.plainCloneHTTP(folder,"https://github.com/dgzlopes/xk6-redis.git","myapikey"))
+  console.log(git.plainCloneSSH(folder,"ssh://git@0.0.0.0:22222/xk6-git","/home/codespace/.ssh/id_rsa"))
 }
