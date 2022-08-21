dc9c5db9246ca0d510b825a73498a4d76a7a1c78
Sean Keery <skibum55@hotmail.com>
2022-08-21 18:55:08 +0000

    updated actions to reflect gitflow branches

--- example.js
+++ example.js
@@ -5,7 +5,7 @@ export default function () {
   var folder = "myfolder" + r
   console.log(folder)
   // console.log("Debug test")
-  //  pick one or both of the methods below depending on your use case
+  //  pick one or both of the methods below depending on your use case - shallow clones
    console.log(git.plainCloneHTTP(folder,"https://github.com/dgzlopes/xk6-redis.git","myapikey",true,1))
   //console.log(git.plainCloneSSH(folder,"ssh://git@0.0.0.0:22222/xk6-git","/home/codespace/.ssh/id_rsa",false,1))
 }
